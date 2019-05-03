623e08dfd71242615adaf3d46231341131c57bb1
Damon Janis <damonvjanis@gmail.com>
2019-05-02 21:29:46 -0600

    Remove unused config

--- config/prod.exs
+++ config/prod.exs
@@ -11,8 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :objective, ObjectiveWeb.Endpoint,
   url: [scheme: "https", host: "damon-objective", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
